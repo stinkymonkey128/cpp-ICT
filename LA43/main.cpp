58f08b0969dec8863539dd2635333b71283fa7b8
MonkeyyBanana <104471645+MonkeyyBanana@users.noreply.github.com>
2022-11-30 00:20:49 -0800

    Update main.cpp

--- LA43/main.cpp
+++ LA43/main.cpp
@@ -5,41 +5,72 @@
 using namespace std;
 
 struct Item {
-   int id, inv;
-   Item* next;
+    int id, inv;
+    Item* next;
 
-   Item(int, int, Item*);
+    Item(int, int, Item*);
 };
 
-Item::Item(int _id, int _inv, Item* _next) : 
-   id(_id), inv(_inv), next(_next) {}
+Item::Item(int _id, int _inv, Item* _next) :
+    id(_id), inv(_inv), next(_next) {}
 
 int CAPACITY = 400;
+int PRIME = 48889;
 
 int main() {
-   Item* hashMap = (Item*) malloc(sizeof(Item) * CAPPACITY * 1.5);
-   ifstream fr("file400.txt");
-   int size = 0, unique = 0;
-   fr >> size;
+    //Item** hashMap = new Item*[CAPACITY * 1.5];//(Item*)malloc(sizeof(Item) * CAPACITY * 1.5);
+    Item* hashMap[600] = {};
+    ifstream fr("file400.txt");
+    int size = 0, unique = 0;
+    fr >> size;
+    
+    for (int i = 0; i < size; i++) {
+        int id, inv, hash = 0;
+        fr >> id >> inv;
 
-   for (int i = 0; i < size; i++) {
-      int id, inv, hash = 0;
-      fr >> id >> inv;
+        string data = to_string(id);
 
-      string data = to_string(id);
+        // Some hashing algo
 
-      // Some hashing algo
+        for (auto& cd : data)
+            hash = cd + (PRIME * hash) - 13;
+        hash = abs((hash + PRIME) % 600);
 
-      // End some hashing algo
+        // End some hashing algo
 
-      hashMap[hash] = new Item(id, inv, hashMap[hash]); // prob pointer error
-   }
+        Item* newItem = new Item(id, inv, hashMap[hash]);
+        hashMap[hash] = newItem;
 
-   fr.close();
+    }
 
-   for (int i = 0; i < CAPACITY * 1.5; i++)
-      if (hashMap[i]->next == NULL)
-         unique++;
+    fr.close();
 
-   cout << "Unique rate = " << (unique / 400) << endl;
+    int longest = 0;
+    double averageLen = 0;
+
+
+    for (int i = 0; i < 600; i++) {
+        if (hashMap[i] == NULL)
+            unique++;
+        else
+        {
+            int length = 1;
+            Item* current = hashMap[i];
+
+            while (current != NULL) {
+                current = current->next;
+                length++;
+            }
+
+            averageLen += length;
+
+            longest = max(longest, length);
+        }
+    }
+
+    averageLen /= (600 - (double)unique);
+
+    cout << "Null Percentage = " << ((double)unique / 600 * 100) << endl;
+    cout << "Average length of linked lists = " << averageLen << endl;
+    cout << "The longested linked list = " << longest;
 }
